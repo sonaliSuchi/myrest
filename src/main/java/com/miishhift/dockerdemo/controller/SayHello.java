8542a002c53ddd54f3aa4b8c16ed28d629dd5f76
sonaliSuchi <57877252+sonaliSuchi@users.noreply.github.com>
2021-05-04 09:20:07 +0200

    ok

--- src/main/java/com/miishhift/dockerdemo/controller/SayHello.java
+++ src/main/java/com/miishhift/dockerdemo/controller/SayHello.java
@@ -17,6 +17,6 @@ public class SayHello {
 	
 		String hello="test";
 		hello=env.getProperty("hello.test");
-		return "Hi!! Your Docker Build pipeline created- Testing openshift webhook 1 "+hello;
+		return "Hi!! Your Docker Build pipeline created- Testing openshift webhook 2 "+hello;
 	}
 }
